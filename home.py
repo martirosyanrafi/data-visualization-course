558b1b1490ce7a0927ec82e839505d9290886095
Rafi <martirosyanrafi@gmail.com>
2021-05-16 14:26:51 +0400

    initial files

---
+++ home.py
@@ -0,0 +1,5 @@
+import dash_html_components as html
+
+
+def get_html():
+    return html.P("This is the content of the home page!")
---
+++ sidebar.py
@@ -0,0 +1,25 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import styles
+
+
+def get_html():
+    html.Div(
+        [
+            html.H2("Sidebar", className="display-4"),
+            html.Hr(),
+            html.P(
+                "A simple sidebar layout with navigation links", className="lead"
+            ),
+            dbc.Nav(
+                [
+                    dbc.NavLink("Home", href="/", active="exact"),
+                    dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                    dbc.NavLink("Page 2", href="/page-2", active="exact"),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+        ],
+        style=styles.SIDEBAR_STYLE,
+    )
